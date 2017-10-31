8e7abfe1215e68605e4da2b07dcb2625aac25693
George Simos <georgesimos@hotmail.gr>
2017-10-31 18:01:34 +0200

    callbacks functions and validation
    add settings callbacks functions
    add validate plugin settings

--- admin/settings-callbacks.php
+++ admin/settings-callbacks.php
@@ -7,16 +7,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 }
 
-
-// validate plugin settings
-function myplugin_validate_options($input) {
-
-	// todo: add validation functionality..
-
-	return $input;
-
-}
-
 // callback: login section
 function myplugin_callback_section_login() {
 
@@ -34,12 +24,19 @@ function myplugin_callback_section_admin() {
 }
 
 
+
 // callback: text field
 function myplugin_callback_field_text( $args ) {
 
-	// todo: add callback functionality..
+	$options = get_option( 'myplugin_options', myplugin_options_default() );
 
-	echo 'This will be a text field.';
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$value = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+	echo '<input id="myplugin_options_'. $id .'" name="myplugin_options['. $id .']" type="text" size="40" value="'. $value .'"><br />';
+	echo '<label for="myplugin_options_'. $id .'">'. $label .'</label>';
 
 }
 
@@ -48,20 +45,48 @@ function myplugin_callback_field_text( $args ) {
 // callback: radio field
 function myplugin_callback_field_radio( $args ) {
 
-	// todo: add callback functionality..
+	$options = get_option( 'myplugin_options', myplugin_options_default() );
 
-	echo 'This will be a radio field.';
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$selected_option = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+	$radio_options = array(
+
+		'enable'  => 'Enable custom styles',
+		'disable' => 'Disable custom styles'
+
+	);
+
+	foreach ( $radio_options as $value => $label ) {
+
+		$checked = checked( $selected_option === $value, true, false );
+
+		echo '<label><input name="myplugin_options['. $id .']" type="radio" value="'. $value .'"'. $checked .'> ';
+		echo '<span>'. $label .'</span></label><br />';
+
+	}
 
 }
+
 
 
 
 // callback: textarea field
 function myplugin_callback_field_textarea( $args ) {
 
-	// todo: add callback functionality..
+	$options = get_option( 'myplugin_options', myplugin_options_default() );
 
-	echo 'This will be a textarea.';
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$allowed_tags = wp_kses_allowed_html( 'post' );
+
+	$value = isset( $options[$id] ) ? wp_kses( stripslashes_deep( $options[$id] ), $allowed_tags ) : '';
+
+	echo '<textarea id="myplugin_options_'. $id .'" name="myplugin_options['. $id .']" rows="5" cols="50">'. $value .'</textarea><br />';
+	echo '<label for="myplugin_options_'. $id .'">'. $label .'</label>';
 
 }
 
@@ -70,9 +95,15 @@ function myplugin_callback_field_textarea( $args ) {
 // callback: checkbox field
 function myplugin_callback_field_checkbox( $args ) {
 
-	// todo: add callback functionality..
+	$options = get_option( 'myplugin_options', myplugin_options_default() );
 
-	echo 'This will be a checkbox.';
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$checked = isset( $options[$id] ) ? checked( $options[$id], 1, false ) : '';
+
+	echo '<input id="myplugin_options_'. $id .'" name="myplugin_options['. $id .']" type="checkbox" value="1"'. $checked .'> ';
+	echo '<label for="myplugin_options_'. $id .'">'. $label .'</label>';
 
 }
 
@@ -81,8 +112,36 @@ function myplugin_callback_field_checkbox( $args ) {
 // callback: select field
 function myplugin_callback_field_select( $args ) {
 
-	// todo: add callback functionality..
+	$options = get_option( 'myplugin_options', myplugin_options_default() );
 
-	echo 'This will be a select menu.';
+	$id    = isset( $args['id'] )    ? $args['id']    : '';
+	$label = isset( $args['label'] ) ? $args['label'] : '';
+
+	$selected_option = isset( $options[$id] ) ? sanitize_text_field( $options[$id] ) : '';
+
+	$select_options = array(
+
+		'default'   => 'Default',
+		'light'     => 'Light',
+		'blue'      => 'Blue',
+		'coffee'    => 'Coffee',
+		'ectoplasm' => 'Ectoplasm',
+		'midnight'  => 'Midnight',
+		'ocean'     => 'Ocean',
+		'sunrise'   => 'Sunrise',
+
+	);
+
+	echo '<select id="myplugin_options_'. $id .'" name="myplugin_options['. $id .']">';
+
+	foreach ( $select_options as $value => $option ) {
+
+		$selected = selected( $selected_option === $value, true, false );
+
+		echo '<option value="'. $value .'"'. $selected .'>'. $option .'</option>';
+
+	}
+
+	echo '</select> <label for="myplugin_options_'. $id .'">'. $label .'</label>';
 
 }
